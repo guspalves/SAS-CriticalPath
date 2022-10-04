155f20fdb270590af08e4425ec7a086ccc34c2e4
guspalves <gus.alves@hotmail.com>
2022-10-04 13:04:38 -0600

    Reading in command line arguments into project

--- main.cpp
+++ main.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
 int main(int argc, char* argv[]){
-    cout << "Hello world!" << endl;
+    if(argc != 4){
+        cerr << "Error: Should have 3 parameters passed into SIM. Check project description to determine the 3 inputs." << endl;
+        exit(-1);
+    }
+
+    // reading in file names for SIM
+    string randNumFileName = argv[1];
+    int numReplications = stoi(argv[2]);
+    string sanFileName = argv[3];
 }
