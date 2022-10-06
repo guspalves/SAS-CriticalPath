a256df2f479b85ffdfe12b17df9e446cb07fd588
guspalves <gus.alves@hotmail.com>
2022-10-05 22:14:46 -0600

    Generating all possible paths in graph

--- main.cpp
+++ main.cpp
@@ -2,15 +2,20 @@
 #include <fstream>
 #include <vector>
 #include <map>
+#include <float.h>
+
 
 using namespace std;
 
-double readToken(ifstream randFile);
+ifstream randNums;
+ifstream sanFile;
 
-struct GraphNode{
-    int val;
-    bool beenVisited = false;
-};
+vector< vector<int> > possiblePaths;
+map<int, vector< pair<int, int> > > graph;
+
+double readNextNum();
+void findPaths(int origin, int dest);
+void DFS(int origin, int dest, vector<int>& path);
 
 int main(int argc, char* argv[]){
     if(argc != 4){
@@ -23,25 +28,79 @@ int main(int argc, char* argv[]){
     int numReplications = stoi(argv[2]);
     string sanFileName = argv[3];
 
-    ifstream randNums(randNumFileName);
+    randNums.open(randNumFileName);
     if(randNums.fail()){
         cerr << "Error: Failure opening file with random values." << endl;
         exit(-1);
     }
 
-    ifstream sanFile(sanFileName);
+    sanFile.open(sanFileName);
     if(sanFile.fail()){
         cerr << "Error: Failure opening SAN description file." << endl;
         exit(-1);
     }
 
     double origin, dest, upper_bound;
-
-    map<int, vector< pair<int, int> > > graph;
+    double min(DBL_MAX), max(-DBL_MAX);
 
     while(sanFile >> origin >> dest >> upper_bound){
         pair<int, int> newPair(dest, upper_bound);
 
         graph[origin].push_back(newPair);
+        graph[dest];
+
+        if(origin < min){
+            min = origin;
+        }
+        if(dest < min){
+            min = dest;
+        }
+
+        if(origin > max){
+            max = origin;
+        }
+        if (dest > max){
+            max = dest;
+        }
+    }
+
+    // closing san file since we no longer need to read in the paths
+    sanFile.close();
+
+    findPaths(min, max);
+    cout << min << " " << max << endl;
+
+    for(auto x : possiblePaths){
+        cout << "Path: ";
+        for(auto node : x){
+            cout << node << " ";
+        }
+        cout << endl;
+    }
+}
+
+double readNextNum(){
+    double test = 0.0;
+    if(randNums >> test) return test;
+    else exit(1);
+}
+
+void findPaths(int origin, int dest){
+    possiblePaths.clear();
+
+    vector<int> currPath;
+    currPath.push_back(origin);
+    DFS(origin, dest, currPath);
+}
+
+void DFS(int origin, int dest, vector<int>& path){
+    if(origin == dest){
+        possiblePaths.push_back(path);
+    } else {
+        for (auto node : graph[origin]){
+            path.push_back(node.first);
+            DFS(node.first, dest, path);
+            path.pop_back();
+        }
     }
 }
