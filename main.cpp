9ac23dfa1e4cb404072bd92e46881c8fc372eb5c
guspalves <gus.alves@hotmail.com>
2022-10-05 19:31:39 -0600

    Reading in graph and storing information in map

--- main.cpp
+++ main.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
+#include <map>
 
 using namespace std;
+
+double readToken(ifstream randFile);
+
+struct GraphNode{
+    int val;
+    bool beenVisited = false;
+};
 
 int main(int argc, char* argv[]){
     if(argc != 4){
@@ -9,8 +18,30 @@ int main(int argc, char* argv[]){
         exit(-1);
     }
 
-    // reading in file names for SIM
+    // reading in file names for SIM and opening files
     string randNumFileName = argv[1];
     int numReplications = stoi(argv[2]);
     string sanFileName = argv[3];
+
+    ifstream randNums(randNumFileName);
+    if(randNums.fail()){
+        cerr << "Error: Failure opening file with random values." << endl;
+        exit(-1);
+    }
+
+    ifstream sanFile(sanFileName);
+    if(sanFile.fail()){
+        cerr << "Error: Failure opening SAN description file." << endl;
+        exit(-1);
+    }
+
+    double origin, dest, upper_bound;
+
+    map<int, vector< pair<int, int> > > graph;
+
+    while(sanFile >> origin >> dest >> upper_bound){
+        pair<int, int> newPair(dest, upper_bound);
+
+        graph[origin].push_back(newPair);
+    }
 }
